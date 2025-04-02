60ad9e457f893a8ebd198ddd1a3eec33038a1e01
Laura Castro <lauracastrotech@gmail.com>
2025-04-02 17:42:18 -0400

    passes wave 4

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -26,6 +26,16 @@ class Vendor:
       self.remove(my_item)
       other_vendor.remove(their_item)
       return True
+   
+   def swap_first_item(self, other_vendor):
+      if not self.inventory or not other_vendor.inventory:
+         return False
+      my_item = self.inventory[0]
+      their_item = other_vendor.inventory[0]
+      return self.swap_items(other_vendor, my_item, their_item)
+
+      
+   
       
       
 
